e22c0ef2e746ef6b42a71a3511721a25ea38ac34
menusha95 <menusha69@gmail.com>
2017-04-23 08:32:57 +0530

    Add existing file

---
+++ app/src/main/java/com/example/menusha/chordx/LearMode.java
@@ -0,0 +1,19 @@
+package com.example.menusha.chordx;
+
+import android.os.Bundle;
+import android.support.v4.app.BundleCompat;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by menusha on 4/23/17.
+ */
+
+public class LearMode extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
